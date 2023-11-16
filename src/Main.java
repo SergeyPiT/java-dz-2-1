19e7758210dc5d83aecc9494dea509011f394e4f
Sergey Ponomarev <itsergeyp@gmail.com>
2023-11-16 20:47:54 +1000

    add new project

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 10475;
+        int rubMile = 20;
+        int bonus = price / rubMile;
+        System.out.println("Вам начислено " + bonus + " миль");
+    }
+}
